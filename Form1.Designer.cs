e74ab8a83f985a063414e1b6e9070f98796e3373
Carlos Emilio Cabral <Carlos Emilio Cabral@DESKTOP-UBTP0PR>
2023-05-03 17:25:41 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Reproductor_VLC_WF_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.btnVerArchivo = new System.Windows.Forms.Button();
+            this.vlcControl1 = new Vlc.DotNet.Forms.VlcControl();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.vlcControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(62, 2);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(75, 33);
+            this.btnOrdenar.TabIndex = 0;
+            this.btnOrdenar.Text = "ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // btnVerArchivo
+            // 
+            this.btnVerArchivo.Location = new System.Drawing.Point(644, 2);
+            this.btnVerArchivo.Name = "btnVerArchivo";
+            this.btnVerArchivo.Size = new System.Drawing.Size(94, 33);
+            this.btnVerArchivo.TabIndex = 1;
+            this.btnVerArchivo.Text = "Ver Archivo";
+            this.btnVerArchivo.UseVisualStyleBackColor = true;
+            this.btnVerArchivo.Click += new System.EventHandler(this.btnVerArchivo_Click);
+            // 
+            // vlcControl1
+            // 
+            this.vlcControl1.BackColor = System.Drawing.Color.Black;
+            this.vlcControl1.Location = new System.Drawing.Point(94, 41);
+            this.vlcControl1.Name = "vlcControl1";
+            this.vlcControl1.Size = new System.Drawing.Size(616, 349);
+            this.vlcControl1.Spu = -1;
+            this.vlcControl1.TabIndex = 2;
+            this.vlcControl1.Text = "vlcControl1";
+            this.vlcControl1.VlcLibDirectory = ((System.IO.DirectoryInfo)(resources.GetObject("vlcControl1.VlcLibDirectory")));
+            this.vlcControl1.VlcMediaplayerOptions = null;
+            this.vlcControl1.Click += new System.EventHandler(this.vlcControl1_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(62, 396);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(109, 42);
+            this.btnPlay.TabIndex = 3;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(644, 396);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(94, 42);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(324, 396);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.vlcControl1);
+            this.Controls.Add(this.btnVerArchivo);
+            this.Controls.Add(this.btnOrdenar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.vlcControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.Button btnVerArchivo;
+        private Vlc.DotNet.Forms.VlcControl vlcControl1;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Reproductor_VLC_WF_CSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Obtener la ruta de la carpeta seleccionada
+                string selectedPath = folderDialog.SelectedPath;
+
+                // Crear carpetas para cada tipo de archivo si no existen
+                string imagesFolder = Path.Combine(selectedPath, "Imágenes");
+                if (!Directory.Exists(imagesFolder))
+                {
+                    Directory.CreateDirectory(imagesFolder);
+                }
+
+                string videosFolder = Path.Combine(selectedPath, "Videos");
+                if (!Directory.Exists(videosFolder))
+                {
+                    Directory.CreateDirectory(videosFolder);
+                }
+
+                string musicFolder = Path.Combine(selectedPath, "Música");
+                if (!Directory.Exists(musicFolder))
+                {
+                    Directory.CreateDirectory(musicFolder);
+                }
+
+                string otherFolder = Path.Combine(selectedPath, "Otros");
+                if (!Directory.Exists(otherFolder))
+                {
+                    Directory.CreateDirectory(otherFolder);
+                }
+                // Obtener una lista de todos los archivos en la carpeta seleccionada
+                string[] files = Directory.GetFiles(selectedPath);
+
+                // Mover los archivos a las carpetas correspondientes
+                foreach (string file in files)
+                {
+                    string extension = Path.GetExtension(file);
+                    string fileName = Path.GetFileName(file);
+
+                    if (extension.Equals(".jpg") || extension.Equals(".jpeg") || extension.Equals(".png") || extension.Equals(".gif"))
+                    {
+                        string destFile = Path.Combine(imagesFolder, fileName);
+                        File.Move(file, destFile);
+                    }
+                    else if (extension.Equals(".mp4") || extension.Equals(".avi") || extension.Equals(".mov") || extension.Equals(".wmv"))
+                    {
+                        string destFile = Path.Combine(videosFolder, fileName);
+                        File.Move(file, destFile);
+                    }
+                    else if (extension.Equals(".mp3") || extension.Equals(".wav") || extension.Equals(".flac"))
+                    {
+                        string destFile = Path.Combine(musicFolder, fileName);
+                        File.Move(file, destFile);
+                    }
+                    else  
+                    {
+                        string destFile = Path.Combine(otherFolder, fileName);
+                        File.Move(file, destFile);
+                    }
+                }
+            }
+
+        
+        }
+
+        private void vlcControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVerArchivo_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Archivos multimedia|*.*";
+            DialogResult result = openFileDialog.ShowDialog();
+
+            try
+            {
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(openFileDialog.FileName))
+                {
+                    string filePath = openFileDialog.FileName;
+                    
+                    vlcControl1.Play(new Uri(filePath));
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (btnPlay.Text == "Pause")
+            {
+                vlcControl1.Pause();
+                btnPlay.Text = "Play";
+            }
+            else if (btnPlay.Text == "Play")
+            {
+                vlcControl1.Play();
+                btnPlay.Text = "Pause";
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            vlcControl1.Stop();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            vlcControl1.Audio.Volume = trackBar1.Value * 10;
+        }
+    }
+}
